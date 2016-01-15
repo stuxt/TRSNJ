2456a0a0ecd605e9e521c237f1c4c2be9b7acf15
stuxt <yajun.chen312@gmail.com>
2016-01-15 17:07:16 +0800

    modify  plugin-page-trs
    修改了js文件

--- 前端插件库/分页插件/TRS.jquery.pagation.v1.0.0.js
+++ 前端插件库/分页插件/TRS.jquery.pagation.v1.0.0.js
@@ -9,7 +9,7 @@
 	// Name:ajax异步加载更多
 	// Author:chen.yajun@trs.com.cn
 	// Requires: jQuery
-	// LastModify:2015-11-11
+	// LastModify:2016-01-15
 	*********************************************************** */ 
 
 	 $.fn.TRS_Page = function(options) {
@@ -21,10 +21,10 @@
 
 		// 默认设置defaults
 		var defaults = { 
-			dataPageName: 'data',//加载数据文件的路径
+			dataPageName: 'data',//加载数据文件的文件名
 			//innerDataId: $("#Data"),//数据加载的目标容器
 			innerPageId: $("#Page"),//加载按钮目标容器
-			pageCount:null,//分数总数
+			pageCount:null,//分页总数
 		    pageIndex:0,//当前页数
 		    pageText:'加载更多'//设置分页文字内容
 		};
@@ -105,32 +105,3 @@
 	  }; 
 
   })(jQuery);
-
-
-	/*
-	 QD.Page v1.0.0
-	 =================================
-
-	 Infomation
-	 ----------------------
-	 Author : lipengyang
-	 E-Mail : lipengyang@trs.com.cn
-	 Date : 2004-01-24
-	 Readme:分页效果
-
-	 Example
-	 ----------------------
-	 pram1:	//分页数据URL
-	 pram2:	//分页输入选择器
-	 pram3:	//数据输入选择器
-	 insertItem: //定义分页数据样式
-
-	 QD.Page('data/data[n].xml','#page','#data');
-	 function insertItem(data) {
-	 return "<li><a href=\""+$(data).attr('url')+"\" title=\""+$(data).attr('title')+"\" target=\"_blank\">"+$(data).text()+"</a></li>";
-	 }
-
-	 Supported in Internet Explorer, Mozilla Firefox
-	 */
-	
-
